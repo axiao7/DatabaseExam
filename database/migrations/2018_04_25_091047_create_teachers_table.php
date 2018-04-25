4c2e3a4f101217a22c249664a9f237232226f2a7
刘文栖 <i@liuwenxi.me>
2018-04-25 17:13:24 +0800

    教师模型以及对应迁移文件

---
+++ database/migrations/2018_04_25_091047_create_teachers_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTeachersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('teachers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('admin_id');
+            $table->string('password');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('teachers');
+    }
+}
