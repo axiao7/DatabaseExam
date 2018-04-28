5b903d86b3340aefa17bb215db889a400b8ee384
yinzhentj <572003212@qq.com>
2018-04-28 10:25:45 +0800

    try

--- resources/views/common/layoutTeacher.blade.php
+++ resources/views/common/layoutTeacher.blade.php
@@ -32,7 +32,26 @@
 {{--</ul>--}}
 {{--</div>--}}
 
+<div class="container">
+    <div class="row" style="margin-top: 50px">
 
+        <!-- 左侧菜单区域   -->
+        <div class="col-md-3">
+            @section('leftmenu')
+                <div class="list-group">
+                    <a href="{{ url('admin') }}" class="list-group-item active">试卷管理</a>
+                    <a href="{{ url('admin/studentinfo') }}" class="list-group-item">学生信息导入</a>
+                    <a href="{{ url('admin/teacherinfo') }}" class="list-group-item">教师信息导入</a>
+                </div>
+            @show
+        </div>
+
+        <!-- 右侧内容区域 -->
+        <div class="col-md-9">
+            试卷信息
+        </div>
+    </div>
+</div>
 
 
 
